d81c31b4ea245becb300a93ea3f751d1b0226d65
kelvin-0 <113959819+kelvin-0@users.noreply.github.com>
2023-07-11 17:12:49 +0700

    removing use client to prevent errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Metadata } from "next";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
-
-export const metadata: Metadata = {
-  title: "Kelvin's Portfolio",
-};
 
 export default function Home() {
   return (
